239dbf7debab27aeb4a2bf75f4e30447548ca614
Lisias T <me@lisias.net>
2022-06-20 23:15:38 -0300

    More orthodoxous deployment model. Updating automation scripts

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
+# see http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
 source ./CONFIG.inc
+IFS=$' '
 
 check() {
 	if [ ! -d "./GameData/$TARGETBINDIR/" ] ; then
 		rm -f "./GameData/$TARGETBINDIR/"
 		mkdir -p "./GameData/$TARGETBINDIR/"
 	fi
+
+	for dll in $EXT_DLLS ; do
+		if [ ! -f "${LIB}/$dll.dll" ] ; then
+			echo "$dll not found!!! Aborting."
+			exit -1
+		fi
+	done
 }
 
 deploy_dev() {
@@ -21,28 +32,84 @@ deploy() {
 	local DLL=$1.dll
 
 	if [ -f "./bin/Release/$DLL" ] ; then
-		cp "./bin/Release/$DLL" "./GameData/$TARGETBINDIR/"
+		cp -R "./bin/Release/$DLL" "./GameData/$TARGETBINDIR/"
 		if [ -d "${KSP_DEV}/GameData/$TARGETBINDIR/" ] ; then
-			cp "./bin/Release/$DLL" "${KSP_DEV/}GameData/$TARGETBINDIR/"
+			cp -R "./bin/Release/$DLL" "${KSP_DEV}/GameData/$TARGETBINDIR/"
 		fi
 	fi
 	if [ -f "./bin/Debug/$DLL" ] ; then
 		if [ -d "${KSP_DEV}/GameData/$TARGETBINDIR/" ] ; then
-			cp "./bin/Debug/$DLL" "${KSP_DEV}GameData/$TARGETBINDIR/"
+			cp -R "./bin/Debug/$DLL" "${KSP_DEV}GameData/$TARGETBINDIR/"
 		fi
 	fi
 }
 
-VERSIONFILE=$PACKAGE.version
+deploy_plugindata() {
+	local DLL=$1.dll
+
+	if [ -f "./bin/Release/$DLL" ] ; then
+		cp "./bin/Release/$DLL" "./GameData/$TARGETBINDIR/PluginData/"
+		if [ -d "${KSP_DEV}/GameData/" ] ; then
+			cp "./bin/Release/$DLL" "${KSP_DEV}GameData/$TARGETBINDIR/PluginData/"
+		fi
+	fi
+	if [ -f "./bin/Debug/$DLL" ] ; then
+		if [ -d "${KSP_DEV}/GameData/" ] ; then
+			cp "./bin/Debug/$DLL" "${KSP_DEV}GameData/$TARGETBINDIR/PluginData/"
+		fi
+	fi
+}
+
+deploy_gamedata() {
+	local PLACE=$1
+	local DLL=$2.dll
+
+	if [ -f "./bin/Release/$DLL" ] ; then
+		cp "./bin/Release/$DLL" "./GameData/000_$DLL"
+		if [ -d "${KSP_DEV}/GameData/" ] ; then
+			cp "./bin/Release/$DLL" "${KSP_DEV/}GameData/${PLACE}_$DLL"
+		fi
+	fi
+	if [ -f "./bin/Debug/$DLL" ] ; then
+		if [ -d "${KSP_DEV}/GameData/" ] ; then
+			cp "./bin/Debug/$DLL" "${KSP_DEV}GameData/${PLACE}_$DLL"
+		fi
+	fi
+}
+
+deploy_ext() {
+	local DLL=$1.dll
+
+	if [ -f "$LIB/$DLL" ] ; then
+		cp -R "$LIB/$DLL" "./GameData/$TARGETBINDIR/"
+		if [ -d "${KSP_DEV}/GameData/" ] ; then
+			cp -R "$LIB/$DLL" "${KSP_DEV/}GameData/$TARGETBINDIR/"
+		fi
+	fi
+}
 
 check
-cp $VERSIONFILE "./GameData/$TARGETDIR"
-cp CHANGE_LOG.md "./GameData/$TARGETDIR/$PACKAGE.CHANGE_LOG.md"
-cp README.md  "./GameData/$TARGETDIR/$PACKAGE.README.md"
-cp LICENSE "./GameData/$TARGETDIR/$PACKAGE.LICENSE"
-cp NOTICE "./GameData/$TARGETDIR/$PACKAGE.NOTICE"
 
-for dll in $PACKAGE ; do
+cp $VERSIONFILE "./GameData/$TARGETDIR/"
+cp CHANGE_LOG.md "./GameData/$TARGETDIR/"
+cp README.md "./GameData/$TARGETDIR/"
+cp LICENSE "./GameData/$TARGETDIR/"
+cp NOTICE "./GameData/$TARGETDIR/"
+
+for dll in $GD_DLLS ; do
     deploy_dev $dll
-    deploy $dll
+    deploy_gamedata $GD_PRIORITY $dll
+done
+
+for dll in $PD_DLLS ; do
+    deploy_plugindata $dll
+done
+
+for dll in $DLLS ; do
+    deploy_dev $dll
+    TARGETBINDIR="" deploy $dll
+done
+
+for dll in $EXT_DLLS ; do
+    deploy_ext $dll
 done
